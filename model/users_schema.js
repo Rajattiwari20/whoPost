b18d0f24684a5de54d0a3d72a2ad047da8c441be
Rajattiwari20 <rajattiwari2098@gmail.com>
2021-06-07 09:17:53 +0530

    setting up user schema

---
+++ model/users_schema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type : String,
+        unique : true,
+        required : true,
+    },
+
+    name : {
+        type : String,
+        required : true,
+    },
+
+    userName : {
+        type : String,
+        unique : true,
+        required : true,
+    },
+
+    password : {
+        type : String,
+        required : true,
+    }
+},{
+    timestamps : true
+});
+
+const User =  mongoose.model('User' , userSchema);
+module.exports = User;
