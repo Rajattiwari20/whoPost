ae494d24ec7934b194955d2f49d06eb4bc90dd7f
Rajattiwari20 <rajattiwari2098@gmail.com>
2021-06-28 15:40:45 +0530

    Added following feature

--- server/controllers/users_controller_Api.js
+++ server/controllers/users_controller_Api.js
@@ -13,14 +13,14 @@ module.exports.signUp = async function(req,res){
     try {
         //password
         const salt = await bcrypt.genSalt(7);
-        const hashedPassword = await bcrypt.hash(req.body.password , salt);
+        const hashedPass = await bcrypt.hash(req.body.password , salt);
 
         //new user
         const newUser = new User({
             userName : req.body.userName,
             email : req.body.email,
             name : req.body.name,
-            password : hashedPassword,
+            password : hashedPass,
         })
 
         // save user    
@@ -80,3 +80,65 @@ module.exports.updateUser = async function(req , res){
     }
 }
 
+
+//delete user
+module.exports.deleteUser = async function(req , res){
+
+    if(req.body.userId == req.params.id){
+        
+        try {
+        
+            const user = await User.findByIdAndDelete(req.body.userId);
+            res.status(200).json("Account Deleted")
+        } catch (error) {
+            return  res.status(500).json(error);
+        }
+    }
+
+    else{
+        return res.status(403).json("You can't delete this account")
+    }
+}
+
+// module.exports.findUser = async function(req, res){
+
+//     try {
+//         const user = User.findById(req.params.id);
+//         if(!user){
+//             res.status(404).send("User not found")
+//         }
+
+//         const {password , updatedAt , ...otherProp} = user._doc;
+//         res.status(200).json(otherProp);
+        
+
+//     } catch (error) {
+//         return  res.status(500).json(error);
+//     }
+    
+
+// }
+
+module.exports.follow = async function(req, res){
+
+    if(req.body.userId !== req.params.id){
+
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push: {followers : req.body.userId}})
+                await currentUser.updateOne({$push: {following : req.params.id}})
+                res.status(200).json("user has been following")
+
+            }else{
+                req.send(403).json("Allready follow")
+            }
+        } catch (error) {
+            return  res.status(500).json(error);
+        }
+
+    }else{
+        res.status(403).json("Can't follow yourself")
+    }
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -15,9 +15,18 @@ router.post('/log-in' , userController.logIn)
 //update
 
 router.put('/:id' , userController.updateUser);
+
 //delete
 
+router.delete('/:id' , userController.deleteUser);
+
+//find user
+
+// router.get('/:id' , userController.findUser);
+
 //follow
+
+router.put('/:id/follow' , userController.follow);
 
 //unfollow  
 
